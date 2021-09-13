977b40b106dd6cd43535a63013b246bc624406b7
Francois Janson <jansonfrancois@gmail.com>
2021-09-13 16:20:37 +0200

    Home component logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Home(props) {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  //   pokemon && console.log("pokemon", pokemon.types[0]);
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>Poke-Context</h1>
+      {pokemon && (
+        <div>
+          <p>Name : {pokemon.name}</p>
+          <p>height : {pokemon.height}</p>
+          <p>weight : {pokemon.weight}</p>
+          <p>types : {pokemon.types.map((elem) => `${elem.type.name} `)}</p>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <h1>Navbar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
     </>
   );
 }
